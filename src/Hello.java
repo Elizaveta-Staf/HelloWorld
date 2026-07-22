fc18ff21799e48235db52ff3304758f3068becf1
Elizaveta Stafionova <elizaveta909.i1ina@gmail.com>
2026-07-22 16:42:11 +0300

    ARS1

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        byte varByte = 1;
+        short varShort = 2;
+        int varInt = 3;
+        long varLong = 4L;
+        float varFloat = 5.5f;
+        double varDouble = 6.6;
+        boolean varBoolean = true;
+        char varChar = 'A';
     }
 }
