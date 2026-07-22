c7abbea6217ea405ea92aef0fdb54f0210186c89
Elizaveta Stafionova <elizaveta909.i1ina@gmail.com>
2026-07-22 13:05:38 +0300

    ARS

---
+++ src/Hello2.java
@@ -0,0 +1,9 @@
+class Hello2 {
+    public static void main(String[] args) {
+        System.out.println(priceCalculation(8.50, 2));
+    }
+
+    public static double priceCalculation(double price, int count) {
+        return price * count;
+    }
+}
