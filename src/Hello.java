08ed6b2dc1dad14f4b5732b734e8c1f10b8359f4
Elizaveta Stafionova <elizaveta909.i1ina@gmail.com>
2026-07-28 17:32:25 +0300

    ARS12

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,17 @@
-public class Hello {
+class Hello {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(isWeekend("Saturday")); // true
+        System.out.println(isWeekend("Monday"));   // false
+    }
+
+    public static boolean isWeekend(String weekday) {
+        switch (weekday) {
+            case "Saturday":
+            case "Sunday":
+                return true;
+            default:
+                return false;
+        }
     }
 }
